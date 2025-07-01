72da23439fc0a038aa0bade97cebd114b81efe7e
Advay Patil <advay@spice.ai>
2025-07-01 10:24:13 -0700

    Simulate build

--- scripts/update-version.js
+++
@@ -1,9 +0,0 @@
-#!/bin/node
-
-const fs = require('fs');
-const packageJson = require('../package.json');
-const version = packageJson.version;
-
-const fileContents = `export const VERSION = '${version}';\n`;
-
-fs.writeFileSync('src/version.ts', fileContents);
